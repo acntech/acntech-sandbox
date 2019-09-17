15fde3a1f367709c99c102e9203dc9a5bdbe2b09
Teo <onlyteo@live.com>
2019-09-17 19:29:01 +0200

    Added Thymeleaf and Stateless Session examples

---
+++ spring-boot/simple-thymeleaf/src/main/java/no/acntech/sandbox/controller/DefaultController.java
@@ -0,0 +1,19 @@
+package no.acntech.sandbox.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class DefaultController {
+
+    @GetMapping("/")
+    public ModelAndView indexPage() {
+        return new ModelAndView("index");
+    }
+
+    @GetMapping("/about")
+    public ModelAndView aboutPage() {
+        return new ModelAndView("about");
+    }
+}
