53ecd485ab50745640b5cd7ee1cdbc4731c579cb
Teo <onlyteo@live.com>
2020-08-24 10:03:26 +0200

    Updated versions, added Spring Boot WebFlux OAuth2 example

---
+++ spring-boot/simple-webflux-oauth2-resource-server/src/main/java/no/acntech/sandbox/config/OAuth2ResourceServerWebSecurityConfig.java
@@ -0,0 +1,31 @@
+package no.acntech.sandbox.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@EnableWebFluxSecurity
+public class OAuth2ResourceServerWebSecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(final ServerHttpSecurity httpSecurity) {
+        return httpSecurity.authorizeExchange()
+                .anyExchange().authenticated()
+                .and().oauth2ResourceServer().jwt()
+                .and().and().build();
+    }
+
+    @Bean
+    public WebClient webClient(final ReactiveClientRegistrationRepository clientRegistrationRepository, final ServerOAuth2AuthorizedClientRepository authorizedClientRepository) {
+        ServerOAuth2AuthorizedClientExchangeFilterFunction filter =
+                new ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository, authorizedClientRepository);
+        return WebClient.builder()
+                .filter(filter)
+                .build();
+    }
+}
