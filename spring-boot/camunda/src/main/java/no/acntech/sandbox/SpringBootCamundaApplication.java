f4487d468e64c6b78571e1eec2bbce48b1feed9a
Teo <onlyteo@live.com>
2021-04-08 11:40:02 +0200

    Added Camunda Spring Boot app

---
+++ spring-boot/camunda/src/main/java/no/acntech/sandbox/SpringBootCamundaApplication.java
@@ -0,0 +1,12 @@
+package no.acntech.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCamundaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCamundaApplication.class, args);
+    }
+}
